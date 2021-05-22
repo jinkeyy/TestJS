c94bbb1e09fc224af61a9e8418e3bd5bfc8be5ce
jinkeyy <71177441+jinkeyy@users.noreply.github.com>
2021-05-23 00:19:54 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let a  = prompt("Nhập từ bàn phím số thứ nhất")
+let b  = prompt("nhập số thứ 2")
+function tinhTong(soThuNhat,soThuHai){
+    return soThuHai-soThuNhat
+}
+alert("tong 2 so la"+tinhTong(a,b))
